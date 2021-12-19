be1f37f050e3e300ffae09337f0209fa26f59fa6
orphee-celui-qui-ne-sait-rien <79347623+orphee-celui-qui-ne-sait-rien@users.noreply.github.com>
2021-12-19 14:53:45 -0600

    added multiple lines for word-gen label, organized code

--- script/gui.py
+++ script/gui.py
@@ -1,12 +1,18 @@
 import tkinter as tk
 from model import process_trigrams
+from textwrap import wrap
 
 
 def main():
     def delete_excess_children_widgets():
         children = root.winfo_children()
         if len(children) >= 3:
-            children[-1].destroy()
+            children[-1].pack_forget()
+
+    def manage_excess():
+        text = label_value.get()
+        text = "\n".join(wrap(text, 45))
+        label_value.set(text)
 
     # noinspection PyUnusedLocal
     def my_tracer(*args):
@@ -20,24 +26,33 @@ def main():
             if tail != "not found":
                 label_value.set(new_text + " " + tail)
             else:
-                temp_label = tk.Label(root, text="No matches in model")
                 temp_label.pack()
+        manage_excess()
 
+    def set_window_config():
+        root.title("Word Generator")
+        x_coords, y_coords = int(root.winfo_screenwidth() / 2 - window_width / 2), int(
+            (root.winfo_screenheight() / 2 - window_height / 2))
+        root.geometry(f"{window_width}x{window_height}+{x_coords}+{y_coords}")
+
+    window_width, window_height = 350, 150
+    # -------------- EXECUTION --------------
     trigram_model = process_trigrams()
+    root = tk.Tk()
+    set_window_config()
 
-    root = tk.Tk(className="My tkinter app")
     # creation of linked variable, from entry to label
-
     entry_value = tk.StringVar()
     entry_value.trace("w", my_tracer)
 
     label_value = tk.StringVar()
 
     # creation of both label and entry with shared variable
-    entry = tk.Entry(root, textvariable=entry_value)
+    entry = tk.Entry(root, textvariable=entry_value, width=window_width)
     label = tk.Label(root, textvariable=label_value)
-    label.pack()
-    entry.pack()
+    label.pack(padx=30)
+    entry.pack(pady=30, padx=30)
+    temp_label = tk.Label(root, text="No matches found in model")
 
     root.mainloop()
 
