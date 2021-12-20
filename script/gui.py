5a7ebcda9e0d8e5e6094d2954f8b89d3f983c602
orphee-celui-qui-ne-sait-rien <79347623+orphee-celui-qui-ne-sait-rien@users.noreply.github.com>
2021-12-20 10:46:57 -0600

    GUI adapted to know show probability of other matches

--- script/gui.py
+++ script/gui.py
@@ -6,28 +6,38 @@ from textwrap import wrap
 def main():
     def delete_excess_children_widgets():
         children = root.winfo_children()
-        if len(children) >= 3:
-            children[-1].pack_forget()
+        if len(children) >= 4:
+            no_match_label.grid_forget()
 
-    def manage_excess():
-        text = label_value.get()
+    def manage_excess_label():
+        text = word_gen_label_value.get()
         text = "\n".join(wrap(text, 45))
-        label_value.set(text)
+        word_gen_label_value.set(text)
+
+    def format_likely_words(tails: tuple) -> str:
+        tails, freqs = tails
+        total_freq = sum(freqs)
+        string = [f"{tail}: {round((freq / total_freq)*100, 2)}%" for tail, freq in zip(tails, freqs)]
+        string = "\n".join(string)
+        return string
 
     # noinspection PyUnusedLocal
     def my_tracer(*args):
         del args
-        new_text: str = entry.get().strip()
+        new_text: str = entry_value.get().strip()
         split_text = new_text.split()
-        label_value.set(new_text)
+        word_gen_label_value.set(new_text)
         delete_excess_children_widgets()
         if len(split_text) >= 2 and new_text[-1] != " ":
-            tail = trigram_model.get_tail(split_text[-2:])
-            if tail != "not found":
-                label_value.set(new_text + " " + tail)
+            tails = trigram_model.get_n_tail(split_text[-2:], False)
+            if tails != "not found":
+                main_tail, tails = tails
+                word_gen_label_value.set(new_text + " " + main_tail)
+                tails = format_likely_words(tails)
+                likely_words_label_value.set(tails)
             else:
-                temp_label.pack()
-        manage_excess()
+                no_match_label.grid(row=3, column=0, columnspan=2)
+        manage_excess_label()
 
     def set_window_config():
         root.title("Word Generator")
@@ -35,24 +45,26 @@ def main():
             (root.winfo_screenheight() / 2 - window_height / 2))
         root.geometry(f"{window_width}x{window_height}+{x_coords}+{y_coords}")
 
-    window_width, window_height = 350, 150
+    window_width, window_height = 450, 250
     # -------------- EXECUTION --------------
     trigram_model = process_trigrams()
     root = tk.Tk()
     set_window_config()
 
-    # creation of linked variable, from entry to label
+    # creation of linked variable, from entry to word_gen_label
     entry_value = tk.StringVar()
     entry_value.trace("w", my_tracer)
 
-    label_value = tk.StringVar()
+    word_gen_label_value = tk.StringVar()
 
-    # creation of both label and entry with shared variable
-    entry = tk.Entry(root, textvariable=entry_value, width=window_width)
-    label = tk.Label(root, textvariable=label_value)
-    label.pack(padx=30)
-    entry.pack(pady=30, padx=30)
-    temp_label = tk.Label(root, text="No matches found in model")
+    # creation of both word_gen_label and entry with shared variable
+    word_gen_label = tk.Label(root, textvariable=word_gen_label_value, width=30).grid(row=0, column=0, padx=30)
+    entry = tk.Entry(root, textvariable=entry_value, width=29).grid(row=1, column=0, pady=30, padx=30)
+    no_match_label = tk.Label(root, text="No matches found in model")
+
+    # most likely words label
+    likely_words_label_value = tk.StringVar()
+    likely_words_label = tk.Label(root, textvariable=likely_words_label_value).grid(row=0, column=1)
 
     root.mainloop()
 
